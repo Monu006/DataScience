99cdacd832aa25e3a3032a29ee7d8b7596a33dce
Monu <monu.works06@gmail.com>
2023-02-03 22:58:14 +0530

    initial commit[[O

---
+++ Assignment_30_Jan/Bike_road_tax_price.py
@@ -0,0 +1,1 @@
+# Write a program to accept the cost price of a bike and display the road tax to be paid according to the
---
+++ Assignment_30_Jan/percentage_to_grade.py
@@ -0,0 +1,16 @@
+# Write a program to accept percentage from the user and display the grade according to the following
+
+
+marks = int(input("Enter your percentage:\n"))
+if marks > 90:
+    print("Congrats!! You got 'A' Grade:")
+elif (marks <= 90 and marks >80):
+    print("Excellent!! You got 'B' Grade:")
+elif (marks>=60 and marks<=80):
+    print("Good! You got 'C' Grade:")
+elif (marks<60):
+    print("Do Hard work! you got 'D' Grade")
+else:
+    print('Seems Invalid Input')
+
+
